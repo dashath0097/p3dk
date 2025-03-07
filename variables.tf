d06620dc2fdc49bbb355154d0b1d41cc23c9e9d1
dashath0097 <dashathk0440@gmail.com>
2025-03-07 13:37:19 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,9 @@
 variable "aws_account_ids" {
-  description = "List of AWS Account IDs for integration"
+  description = "List of AWS Account IDs"
   type        = list(string)
+  default     = []
 }
+
 
 variable "spacelift_space_id" {
   description = "The Spacelift space ID"
